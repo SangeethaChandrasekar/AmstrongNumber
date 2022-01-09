0c4f94dfd106b7ecc4edc0e6977da2c3316fe571
B-Gowthami <beerakamgowthami@gmail.com>
2022-01-09 12:08:44 +0530

    Commit

---
+++ src/armstrong/ArmstrongNumber.java
@@ -0,0 +1,19 @@
+package armstrong;
+
+public class ArmstrongNumber {
+    public static void main(String args[]) {
+        int number=1634,temp, remainder, result=0;
+        temp=number;
+        while (temp!=0)
+        {
+          remainder = temp%10;
+          result += Math.pow(remainder,4);
+          temp=temp/10;
+        }
+        if(result == number)
+            System.out.println(number + " is an Armstrong number");
+        else
+            System.out.println(number + " is not an Armstrong number");
+    }
+}
+
